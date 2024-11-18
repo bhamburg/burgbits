5761b3915e3ccefc4669704f2a3299f6660229f1
Brian Hamburg <bhamburg@gmail.com>
2024-11-18 09:28:14 -0500

    cleaned up styles for shelf indicators

--- components/Shelf.vue
+++ components/Shelf.vue
@@ -7,22 +7,23 @@ const { data } = await useFetch<any>(props.api)
 <template>
   <ClientOnly v-for="shelf in data.shelves">
     <h3 class="text-center md:text-left">{{ shelf.title }}</h3>
-    <div class="flex flex-row flex-wrap items-end justify-center md:justify-start relative">
+    <div class="flex flex-row flex-wrap items-end justify-center md:justify-start">
       <NuxtLink v-for="item in shelf.items.slice(0, 17)" 
         :key="item.title"
         :title="
           item.title  
           + (item.author ? ` - ${item.author}` : '') 
           + (item.platforms?.length ? ` - ${item.platforms.at(-1)}` : '')
-          + (item.firstTime ? ' - First Playthrough' : '')
           + (item.completionLevel === 'A' ? ' - 100%' : '')
+          + (item.firstTime ? ' - First Playthrough' : '')
         " 
         :to="item.url"
         class="mx-3 
           mb-10 
           drop-shadow-lg 
           hover:drop-shadow-xl  
-          transition"
+          rounded
+          relative"
         target="_blank"
       >
         <img
@@ -30,11 +31,16 @@ const { data } = await useFetch<any>(props.api)
           :src="item.coverSrc" 
           class="w-24 rounded"
         />
-        <div class="absolute left-1 bottom-0">
-          <span v-if="item.firstTime">✅</span>
+        <div 
+          :class="item.completionLevel === 'A' || item.firstTime ? 'bg-gradient-to-t from-white to-50% opacity-70' : ''" 
+          class="absolute w-full h-full top-0 rounded" 
+        />
+        <div v-if="item.firstTime" class="absolute right-1 bottom-1">
+          🆕
         </div>
-        <div v-if="item.completionLevel === 'A'" 
-          class="absolute right-1 bottom-1 text-center bg-white w-4 h-4 text-sm rounded">💯</div>
+        <div v-if="item.completionLevel === 'A'" class="absolute left-1 bottom-1">
+          💯
+        </div>
       </NuxtLink>
       <NuxtLink v-if="shelf.items.length > 6" :to="shelf.fetchedFrom" target="_blank"
         class="flex items-center text-center font-bold capitalize justify-center h-36 w-24 mx-3 mb-10 rounded
@@ -55,8 +61,12 @@ const { data } = await useFetch<any>(props.api)
     </div>
   </ClientOnly>
   <p>
-    <span v-if="data?.shelves?.at(-1)?.items?.find((item: any) => item.firstTime)">First Playthrough: ✅</span><br />
-    <span v-if="data?.shelves?.at(-1)?.items?.find((item: any) => item.completionLevel === 'M')">100% Completion: 💯</span><br />
+    <span v-if="data?.shelves?.at(-1)?.items?.find((item: any) => item.completionLevel === 'M')">
+      100% Completion: 💯
+    </span><br />
+    <span v-if="data?.shelves?.at(-1)?.items?.find((item: any) => item.firstTime)">
+      First Playthrough: 🆕
+    </span><br />
     <span>Powered by <NuxtLink :to="data.profileUrl">{{ data.appName }}</NuxtLink></span><br />
     <ClientOnly v-if="data.fetched" class="mt-0">
       Last fetched: 
