da9ad74febc59d9409af1f1073e4063d139a67cf
Brian Hamburg <bhamburg@gmail.com>
2025-03-09 23:55:42 -0400

    WIP: table toggle

--- components/Shelf.vue
+++ components/Shelf.vue
@@ -1,27 +1,58 @@
 <script setup lang="ts">
-const props = defineProps(['api'])
+const props = defineProps(['alwaysGrid','alwaysTable','api','grid','table'])
+const isTable = ref(false)
+
+const toggleTable = () => {
+  if (!props.alwaysGrid || !props.alwaysTable) {
+    isTable.value = !isTable.value
+  }
+}
 
 const { data } = await useFetch<any>(props.api)
+
+watchEffect(() => {
+  if (props.alwaysGrid || props.grid) {
+    isTable.value = false
+  }
+})
 </script>
 
 <template>
   <ClientOnly v-for="shelf in data.shelves">
-    <h3 class="text-center md:text-left">{{ shelf.title }}</h3>
-    <div class="flex flex-row flex-wrap items-end justify-center md:justify-start no-underline">
+    <div class="flex justify-between content start">
+      <h3>{{ shelf.title }}</h3>
+      Always grid: {{ props.alwaysGrid }}
+      <button v-if="!props.alwaysGrid || !props.alwaysTable" class="cursor-pointer mb-5 text-zinc-800 dark:text-white hover:text-sky-600 dark:hover:text-indigo-300" @click="toggleTable()">
+        <ClientOnly>
+          <a v-if="isTable" title="view as grid">
+            <svg fill="currentColor" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M128 136c0-22.1-17.9-40-40-40L40 96C17.9 96 0 113.9 0 136l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48zm0 192c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48zm32-192l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM288 328c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48zm32-192l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM448 328c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48z"/>
+            </svg>
+          </a>
+          <a v-if="!isTable" title="view as table">
+            <svg fill="currentColor" width="24" height="24" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M0 96C0 60.7 28.7 32 64 32l384 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L64 480c-35.3 0-64-28.7-64-64L0 96zm64 0l0 64 64 0 0-64L64 96zm384 0L192 96l0 64 256 0 0-64zM64 224l0 64 64 0 0-64-64 0zm384 0l-256 0 0 64 256 0 0-64zM64 352l0 64 64 0 0-64-64 0zm384 0l-256 0 0 64 256 0 0-64z"/>
+            </svg>
+          </a>
+        </ClientOnly>
+        <span class="sr-only">Toggle dark mode</span>
+      </button>
+    </div>
+    <div v-if="!isTable" class="flex flex-row flex-wrap items-end justify-center md:justify-start no-underline">
       <NuxtLink v-for="item in shelf.items.slice(0, 17)" 
         :key="item.title"
         :title="
           item.title  
           + (item.author ? ` - ${item.author}` : '') 
           + (item.platforms?.length ? ` - ${item.platforms.at(-1)}` : '')
-          + (item.completionLevel === 'A' ? ' - 100%' : '')
           + (item.firstTime ? ' - First Playthrough' : '')
+          + (item.completionLevel === 'A' ? ' - 100% Completion' : '')
         " 
         :to="item.url"
         class="mx-3 
           mb-10 
-          drop-shadow-lg 
-          hover:drop-shadow-xl  
+          drop-shadow-md 
+          hover:drop-shadow-lg  
           relative
           no-underline"
         target="_blank"
@@ -31,18 +62,44 @@ const { data } = await useFetch<any>(props.api)
           :src="item.coverSrc" 
           class="w-24 rounded-none"
         />
-        <div v-if="item.completionLevel === 'A'" class="bg-emerald-500 text-white text-[0.5rem] text-center font-mono">
-          100% Completed
-        </div>
-        <div v-if="item.firstTime" class="bg-sky-600 text-white text-[0.5rem] text-center font-mono">
-          First Time
-        </div>
-        <div class="bg-black text-white text-[0.5rem] text-center font-mono">
-          {{ item.dateFinished }}
+        <div v-if="!shelf.title.toLowerCase().includes('current')">
+          <div class="flex">
+            <div v-if="item.firstTime" 
+              :class="item.completionLevel === 'A' ? 'w-1/2' : 'w-full'"
+              class="
+                bg-emerald-500 
+                text-white 
+                text-[0.5rem] 
+                text-center 
+                font-mono
+              ">
+              NEW
+            </div>
+            <div v-if="item.completionLevel === 'A'" 
+              :class="item.firstTime ? 'w-1/2' : 'w-full'"
+              class="
+                bg-sky-600 
+                text-white 
+                text-[0.5rem] 
+                text-center 
+                font-mono
+              ">
+              100%
+            </div>
+          </div>
+          <div class="
+            bg-black
+            text-white
+            text-[0.5rem] 
+            text-center 
+            font-mono
+          ">
+            {{ item.dateFinished }}
+          </div>
         </div>
       </NuxtLink>
       <NuxtLink v-if="shelf.items.length > 6" :to="shelf.fetchedFrom" target="_blank"
-        class="flex items-center text-center font-bold capitalize justify-center h-40 w-24 mx-3 mb-10
+        class="flex items-center text-center font-bold capitalize justify-center h-[156px] w-24 mx-3 mb-10
           bg-gradient-to-l
           hover:bg-gradient-to-r
           text-white 
@@ -51,8 +108,8 @@ const { data } = await useFetch<any>(props.api)
           to-emerald-400 
           dark:from-indigo-900 
           dark:to-black   
-          drop-shadow-lg 
-          hover:drop-shadow-xl
+          drop-shadow-md 
+          hover:drop-shadow-lg
           no-underline"
       >
         View all
@@ -60,7 +117,7 @@ const { data } = await useFetch<any>(props.api)
     </div>
   </ClientOnly>
   <p>
-    <span>Powered by <NuxtLink :to="data.profileUrl">{{ data.appName }}</NuxtLink></span><br />
+    <span>Powered by <NuxtLink :to="data.profileUrl" target="_blank">{{ data.appName }}</NuxtLink></span><br />
     <ClientOnly v-if="data.fetched" class="mt-0">
       Last fetched: 
       {{ new Date(data.fetched).toLocaleDateString('en-us', { 
