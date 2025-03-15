a5de04c7adfa49186f103bdaf80a8cc7af99487a
Brian Hamburg <bhamburg@gmail.com>
2025-03-15 14:03:54 -0400

    style changes

--- components/OnThisPage.vue
+++ components/OnThisPage.vue
@@ -5,7 +5,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="lg:fixed flex flex-col toc lg:w-[190px] mt-8 lg:mt-14 mx-4 lg:bg-zinc-100 lg:dark:bg-zinc-700 lg:p-6 z-1 lg:shadow lg:rounded-lg">
+  <div class="lg:fixed flex flex-col toc lg:w-[190px] mt-8 lg:mt-14 mx-4 mb-0 bg-zinc-100 dark:bg-zinc-700 p-6 z-1 shadow rounded-lg">
     <h2 class="mt-0 mb-1 text-xl">On This Page</h2>
     <ul v-for="link in props.links" class="mb-0">
       <li>
